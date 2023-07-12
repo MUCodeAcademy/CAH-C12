fa066848170de9a6582b255d364dbfa061938a1c
aanzalone356 <andrew.anzalone03@gmail.com>
2023-07-11 19:17:35 -0500

    adds Server file and routes

---
+++ server/routes/auth.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const authServices = require("../services/authenticator");
+
+router.post('/login', authServices.login);
+router.post('/register', authServices.register);
+router.post('/fireAuthSignOn', authServices.fireAuthSignOn);
+
+
+module.exports = router;
