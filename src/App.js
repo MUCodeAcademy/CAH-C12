227e693123873d134d60ff441588030aee296239
Trevor Parks <clank1234@mailfence.com>
2023-07-17 17:47:40 -0500

    update loginpage, registerpage, add multiple files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import LoginPage from "./components/LoginPage.jsx"
 import RegisterPage from "./components/RegisterPage.jsx"
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes, 
+  Route,
+  Navigate,
+} from "react-router-dom";
 import React, {useState, useEffect} from 'react';
+import Menu from "./components/Menu.jsx"
 
 
 function App() {
   return (
     <div className="App">
-      <LoginPage />
-      {/* <RegisterPage /> */}
+      <Router>
+  <Menu />
+    <Routes>
+      <Route path='/' exact element={<LoginPage />}/>
+      <Route path='/register' element={<RegisterPage />}/>
+      {/* <Route path='/lobby' element={<LobbyPage />}/>
+      <Route path='/game' element={<GamesPage />}/> */}
+      <Route path='*' element={<Navigate to="/" />} />
+    </Routes> 
+  </Router>
     </div>
   );
 }
