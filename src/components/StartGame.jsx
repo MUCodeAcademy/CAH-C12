fd50f0271edc5e813956a4786f63ac43f3b6dd39
Shawn <s_wagoner85@yahoo.com>
2023-07-17 19:49:46 -0500

    start game with a timer

--- src/components/StartGame.jsx
+++ src/components/StartGame.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function StartGame() {
   const [players, setPlayers] = useState([
@@ -10,18 +10,51 @@ function StartGame() {
   const [currentJudgeIndex, setCurrentJudgeIndex] = useState(0);
   const [cardsDealt, setCardsDealt] = useState(false);
   const [gameStarted, setGameStarted] = useState(false);
+  const [currentPlayerIndex, setCurrentPlayerIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  useEffect(() => {
+    if (!gameStarted) {
+     
+      setCurrentJudgeIndex(Math.floor(Math.random() * players.length));
+    }
+  }, [gameStarted, players.length]);
+
+  useEffect(() => {
+    let timer;
+
+    if (isPlaying) {
+      timer = setTimeout(() => {
+        handleNextPlayer();
+      }, 20000); 
+
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [isPlaying, currentPlayerIndex]);
 
   const handleStartGame = () => {
     setGameStarted(true);
   };
 
   const handleDealCards = () => {
-    
     setCardsDealt(true);
+    handleNextPlayer(); 
+  };
+
+  const handleNextPlayer = () => {
+    setIsPlaying(false);
+    setCurrentPlayerIndex((prevIndex) => (prevIndex + 1) % players.length);
   };
 
   const handleNextJudge = () => {
     setCurrentJudgeIndex((prevIndex) => (prevIndex + 1) % players.length);
+  };
+
+  const handlePlayCard = () => {
+    console.log(`${players[currentPlayerIndex].name} played a card.`);
+    handleNextPlayer();
   };
 
   return (
@@ -37,6 +70,12 @@ function StartGame() {
         <div>
           <h3>Judge: {players[currentJudgeIndex].name}</h3>
           <button onClick={handleNextJudge}>Next Judge</button>
+          <button onClick={() => setIsPlaying(true)}>Start Timer</button>
+          <button onClick={() => setIsPlaying(false)}>Stop Timer</button>
+          <button onClick={handlePlayCard} disabled={!isPlaying}>
+            Play Card
+          </button>
+          <p>Current Player: {players[currentPlayerIndex].name}</p>
         </div>
       )}
     </div>
