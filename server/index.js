62206336b6d693311a721da6eca381fce6deaf1e
Trevor Parks <clank1234@mailfence.com>
2023-07-17 20:18:14 -0500

    changes

--- server/index.js
+++ server/index.js
@@ -3,11 +3,32 @@ const express = require('express');
 const cors = require('cors');
 const authRoutes = require("../server/routes/auth");
 const app = express();
-const port = process.env.DB_PORT;
+const port = process.env.DB_PORT || 8080;
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+        credentials: true
+    }
+});
+
 
 app.use(cors());
 app.use(express.json());
 app.use('/auth', authRoutes);
+
+io.on("connection", (socket) => {
+    console.log("New Client Connected");
+
+    socket.on("message", (msg) => {
+        io.emit("message", msg);
+    }); 
+    
+    socket.on("disconnect", () => {
+        console.log("Client Disconnected");
+    });
+});
 
 app.listen(port, () => {
     console.log("App is listening at: " + port);
