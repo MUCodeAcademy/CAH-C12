9a81c5ef6745b872679e3af36fb627d2b5153c6b
corewitz <corewitz@gmail.com>
2023-07-20 18:50:52 -0500

    tried to add useSocket.js

--- src/context/index.js
+++
@@ -1,9 +0,0 @@
-import { UserProvider } from './UserContext';
-
-export default function StateProvider(props) {
-    return (
-        <UserProvider>
-            {props.children}
-        </UserProvider>
-    )
-}
