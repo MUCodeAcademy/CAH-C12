f4491379ab9a5084742f2ea371bb1c63add00fe7
aanzalone356 <andrew.anzalone03@gmail.com>
2023-07-26 20:07:28 -0500

    fixes Lobby page

--- src/components/LobbyPage.jsx
+++ src/components/LobbyPage.jsx
@@ -1,6 +1,17 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Button, Box, Grid, Card, CardContent, styled } from '@mui/material';
+import CardActions from '@mui/material/CardActions';
+import Typography from '@mui/material/Typography';
 import { useUserContext } from '../context/UserContext';
-import { useLobbyContext } from '../context/LobbyContext';
+
+const GridItem = styled((props) => (
+  <Grid item xs {...props} /> ))
+  ({
+      margin: 0,
+      rowSpacing: 0.5,
+      columnSpacing: 0.5
+  })
+
 
 function Lobby() {
   const {user} = useUserContext();
@@ -9,18 +20,29 @@ function Lobby() {
     { id: 2, name: 'Table 2' },
     { id: 3, name: 'Table 3' },
   ]);
+
+
   const [selectedTable, setSelectedTable] = useState(null);
-  const { table, joinTable, leaveTable } = useLobbyContext();
-  const handleTableSelect = (tableId) => {
-    setSelectedTable(tableId);
-  };
   const userArray = [];
 
+  const handleTableSelect = (tableId) => {
+    setSelectedTable(tableId);
+    console.log(user.username);
+    userArray.push(user.username);
+    if(userArray.length === 1){
+      //This means the new user is first and the host on there waiting screen display a button to start
+    } else if(userArray.length === 4){
+      //Start the game and route to GamePage
+    }
+  };
+
+  
   const handleJoinTable = () => {
     console.log("player from context: ", user);
-    
+
     if (selectedTable) {
-      joinTable(selectedTable, user);
+      console.log(`Joined table ${selectedTable}`);
+      //joinTable(selectedTable, user);
       console.log(user.username);
       userArray.push(user.username);
       if(userArray.length === 1){
@@ -33,22 +55,54 @@ function Lobby() {
     }
   };
 
-  return (
+  useEffect(() => {
+    console.log(user.username);
+  }, [userArray]);
+
+  //Still deciding whether it should be sepereate cards or not 
+  const Lobby = (
+    <React.Fragment>
+      <CardContent>
+        <Typography variant="h5" component="div">
+          Lobby
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          Lobby
+        </Typography>
+        <Typography variant="body2">
+          <br />
+          <br />
+          Insert Player in lobby here
+          {userArray.map((username, index) => (
+            <GridItem key={index}>{username}</GridItem>
+          ))}
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small" onClick={() => handleTableSelect()}>Join Lobby</Button>
+      </CardActions>
+    </React.Fragment>
+  );
+  
+  return(
     <div>
-      <h2>Lobby</h2>
-      <h3>Available Tables:</h3>
-      <ul>
-        {tables.map((table) => (
-          <li key={table.id}>
-            <button onClick={() => handleTableSelect(table.id)}>
-              {table.name}
-            </button>
-          </li>
-        ))}
-      </ul>
-      <button onClick={handleJoinTable}>Join Table</button>
+          <h1>Lobby</h1>
+      <hr />
+      <Grid container spacing={8} justifyContent="center">
+          <Grid item>
+            <Card variant="outlined">{Lobby}</Card>
+          </Grid>
+          <Grid item>
+            <Card variant="outlined">{Lobby}</Card>
+          </Grid>
+          <Grid item>
+            <Card variant="outlined">{Lobby}</Card>
+          </Grid>
+      </Grid>
+
     </div>
   );
 }
-
 export default Lobby;
