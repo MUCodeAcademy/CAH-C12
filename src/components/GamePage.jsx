ee6f1f7d4846b01b2e470067e8cf6cfa4ab7e852
aanzalone356 <andrew.anzalone03@gmail.com>
2023-07-27 19:13:02 -0500

    working Lobby

--- src/components/GamePage.jsx
+++ src/components/GamePage.jsx
@@ -4,34 +4,59 @@ import Rules from './Rules';
 import WinDisplay from './WinDisplay';
 import GameDisplay from './GameDisplay';
 import  { useGameDisplayContext, useRankContext} from '../context/GameDisplayContext';
+import { useLobbyContext } from '../context/LobbyContext';
+import { useGameContext } from '../reducers/gamePageReducer';
+import { StartGame } from './StartGame';
 
 export default function GamePage(props) {
     const { user } = useUserContext();
-    const started = true; 
-    const finished  = false;
-    const lobbyId = props.lobbyId;
-
-    const {isPlaying, setIsPlaying } = useGameDisplayContext();
-    const {ranks} = useRankContext();
-
+    const { isPlaying, setIsPlaying } = useGameDisplayContext();
+    const { ranks, setRanks } = useRankContext();
+    const { tables } = useLobbyContext();
+    const { started, finished, lobbyId, playersTurn } = useGameContext();
     
+    lobbyId = props.lobbyId;
+    isPlaying = props.isPlaying;
+
+    started = true;
+    finished = false;
+    playersTurn = true;
+
+    const [playerArray, setPlayerArray] = useState([]);
+    setPlayerArray(tables[lobbyId]);
+    setIsPlaying(isPlaying);
+
+
     //TODO: Start game when the player array is recieved (handleStart())
     //TODO: After deciding the inital player state (turns, points, and components)
     //TODO: Pass info off to handleGameState
     const handleStart = () => {
         // Update the game state to indicate that the game has started.
+        StartGame();
+
+        return { playerArray, playersTurn, playersRank };
     };
 
     
     //TODO: This should also send the player array to GameDisplay
     const handleGameState = () => {
         // Handle changes to the game state.
+        handleStart();
+        useEffect(() => {
+            
+        }, [setIsPlaying]);
+
+        return
     };
 
 
     //TODO: When GameDisplay returns the RANKS[] array handleEnding()
     const handleEnding = () => {
         // Handle the end of the game.
+        finished = true;
+        playersTurn = false;
+
+
     };
 
 
@@ -43,7 +68,7 @@ export default function GamePage(props) {
            {started &&  
             <div>
                 {isPlaying 
-                    ?<GameDisplay isPlaying={true}/> 
+                    ?<GameDisplay playerArray={playerArray} isPlaying={true}/> 
                     :<GameDisplay isPlaying={false}/>
                 }
             </div>
